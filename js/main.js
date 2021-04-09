d00f4d5bbd35322ded2490a4c0964834c21061c9
jieun <bje0825@naver.com>
2021-04-10 06:04:51 +0900

    add products page

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@
 	let button = document.querySelector("#button");
 	let burgerCon = document.querySelector("#burgerCon");
 
+
 	function hamburgerMenu() {
 		burgerCon.classList.toggle("slideToggle");
 		button.classList.toggle("expanded");
 	}
-
+	
 	button.addEventListener("click", hamburgerMenu, false);
 
 	let planSelectorNav = document.querySelectorAll("#planSelector li");
 	let membershipPlans = document.querySelectorAll(".membershipPlans");
+
 
 
 })();
